b8123a28ba474b68b297a522266e15f371662231
William Andrews <wtandrews05@gmail.com>
2026-03-17 19:47:56 -0400

    Add compile time arithmetic

--- include/cache.hpp
+++ include/cache.hpp
@@ -14,6 +14,10 @@
 // 64 = 2^6 different offsets.
 // Since the CPU address is 32 bits, 32 = 5 + 6 + 21 implies that there are 21 bits of the tag field.
 
+#define WAY 4
+#define CACHE_SIZE_BYTES 8192
+#define CACHE_BLOCK_SIZE_BYTES 64
+
 struct Address {
     uint32_t tag;           // 21 bits
     uint8_t set;            // 5 bits
@@ -28,20 +32,27 @@ struct CacheLine {
 
 class L1Cache {
     public:
-        L1Cache() = default;
+        L1Cache();
         L1Cache(std::vector<uint8_t> input_data);
 
         uint8_t read(Address address);
         std::vector<uint8_t> batch_read(std::vector<Address> addresses);
 
     private:
-        CacheLine lines_[128];
-        std::random_device rd_;                     // for random eviction (obtains a seed)
+        constexpr int nblocks_;
+        constexpr int nsets_;
+        constexpr int nindices_;
+        constexpr int nblock_offset_bits_;
+        constexpr int ntag_bits_;
+        CacheLine lines_[nblocks_];
+        constexpr int ram_size = 4096; // 4096 is arbitrary size, just don't make tag range too much in the addresses and cache lines
+        uint8_t* ram_[ram_size]; 
+        std::random_device rd_;                    // for random eviction (obtains a seed)
         std::mt19937 gen_;                         // Standard mersenne twister engine seeded with rd()
         std::uniform_int_distribution<> distrib_;  // Define the desired distribution
 
-
         void write(uint8_t input);
         void batch_write(std::vector<uint8_t> input_data);
-        void evict_and_replace(uint8_t set_index);
+        uint8_t* fetch_data(uint32_t ram_index);
+        uint8_t evict_and_replace(Address address);
 };
--- src/cache.cpp
+++ src/cache.cpp
@@ -1,12 +1,30 @@
 #include "cache.hpp"
 
-#define WAY 4
+L1Cache::L1Cache() : nblocks_(CACHE_SIZE_BYTES / CACHE_BLOCK_SIZE_BYTES), 
+    nsets_(nblocks_ / WAY), 
+    nindices_(std::log2(nset_bits_)),
+    nblock_offset_bits_(std::log2(CACHE_BLOCK_SIZE_BYTES)),
+    ntag_bits_(nsets_ - nblock_offset_bits_ - nindices_)
+{
+    // allocate ram
+    for (int i = 0; i < ram_size; i++) 
+        ram_[i] = new int[CACHE_BLOCK_SIZE_BYTES];
+
+    // set up random number generator for random eviction
+    gen_(rd_());
+    distrib_(0, 255); // Define the desired distribution (e.g., integers between 0 and 255 which is uint8_t max)
+}
 
 L1Cache::L1Cache(std::vector<Address> input_data) {
-    gen_(rd_());
-    distrib_(0, 255); // Define the desired distribution (e.g., integers between 1 and 100)
+    // allocate ram
+    for (int i = 0; i < ram_size; i++) 
+        ram_[i] = new int[CACHE_BLOCK_SIZE_BYTES];
     
-
+    // set up random number generator for random eviction
+    gen_(rd_());
+    distrib_(0, 255); // Define the desired distribution (e.g., integers between 0 and 255 which is uint8_t max)
+    
+    // read in data to cache
     batch_read(input_data);
 }
 
@@ -22,7 +40,7 @@ uint8_t L1Cache::read(Address address) {
         }
     }
     // if no tag match found in cache, evict and replace.
-    evict_and_replace(address.set);
+    return evict_and_replace(address);
 }
 
 std::vector<uint8_t> L1Cache::batch_read(std::vector<Address> addresses) {
@@ -30,20 +48,28 @@ std::vector<uint8_t> L1Cache::batch_read(std::vector<Address> addresses) {
         read(address);
 }
 
-void L1Cache::write(uint8_t input) {
+// void L1Cache::write(uint8_t input) {
 
+// }
+
+// void L1Cache::batch_write(std::vector<uint8_t> input_data) {
+
+// }
+
+uint8_t* fetch_data(uint32_t ram_index) {
+    if (ram_index >= ram_size) throw std::out_of_range("Error in RAM data fetch: index out of range.");
+    return ram_[ram_index];
 }
 
-void L1Cache::batch_write(std::vector<uint8_t> input_data) {
-
-}
-
-void L1Cache::evict_and_replace(uint8_t set_index) {
+uint8_t L1Cache::evict_and_replace(Address address) {
+    // Generate the random number mod WAY
+    uint8_t r = static_cast<uint8_t>(distrib_(gen_)) % WAY;
     
-
-    // 2. 
-
-    // 3. 
-    // 4. Generate and print the random number
-    int random_number = distrib(gen);
+    // replace the metadata
+    lines_[address.set + r].valid = true;                               // valid bit set to true
+    lines_[address.set + r].tag = address.tag;                          // tag bit set
+    memcpy(lines_[address.set + r].data, fetch_data(address.tag), 64);  // data copied from lower level
+    
+    // return the found uint8_t
+    return lines_[address.set + r].data[address.block_offset];
 }
